804b865de394cc0c91ec17896e8188abc641ed5e
Erkan TOKMAK <erkan.tokmak.ofc@gmail.com>
2024-07-26 13:24:11 +0300

    Swiper.js installed. Product Types created. Firebase types created. Auth,basket,products store file created.

---
+++ tukcell-pasaj/src/services/product.ts
@@ -0,0 +1,80 @@
+interface Detail {
+    model: string;
+    storage: string;
+    screenSize: string;
+    screenResolution: string;
+    processor: string;
+    ram: string;
+    battery: string;
+    camera: string;
+    frontCamera: string;
+    operatingSystem: string;
+    simCard: string;
+    network: string;
+    weight: string;
+    dimensions: string;
+    waterproof: string;
+    dualSim: string;
+    warranty: string;
+}
+
+interface Comment {
+    id: number;
+    userId: number;
+    userName: string;
+    comment: string;
+    rating: number;
+    like: number;
+    date: string;
+}
+
+interface Question {
+    id: number;
+    userId: number;
+    userName: string;
+    question: string;
+    answer: string;
+    answerBy: string;
+    date: string;
+}
+
+interface Explanation {
+    title: string;
+    text: string;
+    image: string;
+}
+
+interface Product {
+    id: number,
+    name: string,
+    price: number,
+    discountPercentage: number,
+    discountEndDate: string,
+    stock: number,
+    bestOffer: boolean,
+    bestSeller: boolean,
+    newArrival: boolean,
+    shoppingCredit: boolean,
+    freeShipping: boolean,
+    fastDelivery: boolean,
+    payWithInvoice: boolean,
+    installmentInvoice: boolean,
+    limitedSelling: boolean,
+    image: Array<string>,
+    color: Array<string>,
+    credit: boolean,
+    creditMonth: number,
+    creditPrice: string,
+    description: string,
+    category: string,
+    subCategory: string,
+    rating: number,
+    addingDate: string,
+    edittedDate: string,
+    seller: string,
+    detailText: string,
+    explanation: Explanation[],
+    details: Detail[],
+    comments: Comment[],
+    questions: Question[]
+}
